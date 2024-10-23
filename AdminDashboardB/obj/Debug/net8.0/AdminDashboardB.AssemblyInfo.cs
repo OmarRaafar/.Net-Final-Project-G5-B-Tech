e1520a8f41633f83f54954f3272127d06acef754
KhaledAshrafMohammed <109352509+KhaledAshrafMohammed@users.noreply.github.com>
2024-10-23 12:02:09 +0300

    update obj and bin

--- AdminDashboardB/obj/Debug/net8.0/AdminDashboardB.AssemblyInfo.cs
+++ AdminDashboardB/obj/Debug/net8.0/AdminDashboardB.AssemblyInfo.cs
@@ -11,12 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-WebApplication1-d68fb8d1-f3f3-4efd-a77f-92cc276819cc")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminDashboardB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+220c1c9e3fd988acbb85bb2a01e8bb924150008f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e42ecb05776fb5d3ae80902181fb8ba8f402d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminDashboardB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminDashboardB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
