89a1f87af910f533000317ac50b2ddb1ee373bf4
KhaledAshrafMohammed <109352509+KhaledAshrafMohammed@users.noreply.github.com>
2024-10-23 18:25:47 +0300

    order files

---
+++ ApplicationB/Services B/Order/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using DTOsB.OrderBDTOs.PaymentDTO;
+using DTOsB.OrderDTO;
+using DTOsB.Shared;
+
+namespace ApplicationB.Services_B.Order
+{
+    public interface IOrderService
+    {
+        Task<ResultView<AddOrUpdateOrderBDTO>> CreateOrderAsync(AddOrUpdateOrderBDTO orderBDTO);
+        Task<ResultView<AddOrUpdateOrderBDTO>> UpdateOrderAsync(AddOrUpdateOrderBDTO orderBDTO);
+        Task<ResultView<SelectOrderBDTO>> DeleteOrderAsync(int id);
+        Task<SelectOrderBDTO> GetOrderByIdAsync(int id);
+        Task<IEnumerable<SelectOrderBDTO>> GetAllOrdersAsync();
+        
+
+
+    }
+}
---
+++ InfrastructureB/Order/ShippingRepository.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationB.Contracts_B;
+using ApplicationB.Contracts_B.Order;
+using DbContextB;
+using InfrastructureB.General;
+using ModelsB.Order_B;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureB.Order
+{
+    public class ShippingRepository : GenericRepositoryB<ShippingB>, IShippingRepository
+    {
+        public ShippingRepository(BTechDbContext context) : base(context)
+        {
+        }
+
+    }
+}
