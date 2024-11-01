01535cf4b50964789ca1dbdc4effc959f220ce43
KhaledAshrafMohammed <109352509+KhaledAshrafMohammed@users.noreply.github.com>
2024-11-01 10:57:05 +0200

    test

--- AdminDashboardB/Controllers/CategoryController.cs
+++ AdminDashboardB/Controllers/CategoryController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace DTOsB.Controllers
 {
 
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     public class CategoryController : Controller
     {
         private readonly ICategoryService _categoryService;
