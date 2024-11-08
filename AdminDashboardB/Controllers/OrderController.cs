f1122965f96b81f4c46e4c7c44397243f97f45db
menna <mennaallah.elsayed23@gmail.com>
2024-11-08 04:51:11 +0200

    for merge

--- AdminDashboardB/Controllers/OrderController.cs
+++ AdminDashboardB/Controllers/OrderController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DTOsB.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
 
     public class OrderController : Controller
     {
@@ -68,7 +68,7 @@ namespace DTOsB.Controllers
                 CurrentStatus = order.CurrentStatus,
                 ApplicationUserId = "db0a8336-7f0f-416c-90c8-a8dfd01d97f7"
             };
-
+            
             return View("Edit", updateOrder);
         }
 
